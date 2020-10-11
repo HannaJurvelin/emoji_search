f94155eee9fefde8995b3afad00d258233e1fcd8
Hanna Jurvelin <>
2020-10-11 09:12:15 +0300

    Made a new component Collabsible

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Collapsible from './components/Collapsible'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Bg">
+      <header>
+        <h1>Emoji search</h1>
+        <input type="text" id="search"/>
       </header>
+      <div>
+        <p>Tulostusalue</p>
+        <Collapsible emoji=":)" name="Slight Smile"/>
+        <Collapsible emoji=":(" name="2"/>
+        <Collapsible emoji=":|" name="3"/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Collapsible.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Collapsible extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+        this.togglePanel = this.togglePanel.bind(this);
+    }
+    togglePanel(e) {
+        this.setState({ open: !this.state.open })
+    }
+    render() {
+        return (<div>
+            <h2 onClick={(e)=>this.togglePanel(e)}>Slight smile</h2>
+            {
+                this.state.open ? (
+                    <p>A yellow face with simple, open eyes and a thin, closed smile. Conveys a wide range of positive, happy, and friendly sentiments. Its tone can also be patronizing, passive-aggressive, or ironic, as if saying “This is fine” when it’s really not.</p>
+                ) : null
+            }
+        </div>);
+    }
+}
+
+export default Collapsible;
