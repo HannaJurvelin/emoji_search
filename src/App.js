1fbfadd64880522eaa085223bdc85e58034f3a5b
Hanna Jurvelin <>
2020-10-18 16:27:38 +0300

    Fetch data from APIvia Axios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Collapsible from './components/Collapsible'
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Collapsible from "./components/Collapsible";
+const axios = require("axios").default;
+require("dotenv").config();
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://emoji-api.com/emojis?access_key=${process.env.REACT_APP_API_KEY}`
+      )
+      .then(function (response) {
+        // handle success
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }, []);
+
   return (
     <div className="Bg">
       <header>
         <h1>Emoji search</h1>
-        <input type="text" id="search"/>
+        <input type="text" id="search" />
       </header>
-      <div>
-        <p>Tulostusalue</p>
-        <Collapsible emoji=":)" name="Slight Smile"/>
-        <Collapsible emoji=":(" name="2"/>
-        <Collapsible emoji=":|" name="3"/>
-      </div>
+      {data
+        ? data.map((item) => (
+            <Collapsible
+              emoji={item.character}
+              name={item.unicodeName}
+              key={item.unicodeName}
+            />
+          ))
+        : null}
     </div>
   );
 }
--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React from "react";
 
 class Collapsible extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false
-        }
-        this.togglePanel = this.togglePanel.bind(this);
-    }
-    togglePanel(e) {
-        this.setState({ open: !this.state.open })
-    }
-    render() {
-        return (<div>
-            <h2 onClick={(e)=>this.togglePanel(e)}>Slight smile</h2>
-            {
-                this.state.open ? (
-                    <p>A yellow face with simple, open eyes and a thin, closed smile. Conveys a wide range of positive, happy, and friendly sentiments. Its tone can also be patronizing, passive-aggressive, or ironic, as if saying “This is fine” when it’s really not.</p>
-                ) : null
-            }
-        </div>);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+    this.togglePanel = this.togglePanel.bind(this);
+  }
+  togglePanel(e) {
+    this.setState({ open: !this.state.open });
+  }
+  render() {
+    return (
+      <div>
+        <h2 onClick={(e) => this.togglePanel(e)}>
+          {this.props.emoji} {this.props.name}
+        </h2>
+        {this.state.open ? (
+          <p>
+            A yellow face with simple, open eyes and a thin, closed smile.
+            Conveys a wide range of positive, happy, and friendly sentiments.
+            Its tone can also be patronizing, passive-aggressive, or ironic, as
+            if saying “This is fine” when it’s really not.
+          </p>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default Collapsible;
